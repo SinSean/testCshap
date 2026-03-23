64c1cb55e31c96722e5f0fed6b034040a61cb6a6
Sean <sealll40@gmail.com>
2026-03-23 14:13:02 +0800

    feat: member ok

---
+++ testCshap/Controllers/MemberController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testCshap.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult HelloAnnZi()
+        {
+            return View();
+        }
+    }
+}
