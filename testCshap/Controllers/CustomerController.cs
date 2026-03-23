75b87bafa3502dd6c7ddc40694987f444858a7d4
shao <fgh78453@gmail.com>
2026-03-23 14:11:05 +0800

    feat: 新增一個controller

---
+++ testCshap/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testCshap.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
