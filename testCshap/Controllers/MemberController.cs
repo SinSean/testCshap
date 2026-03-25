7bcb3d584de48455b433b885656760e24471a23c
Sean <sealll40@gmail.com>
2026-03-25 11:14:13 +0800

    feat: 完成會員新增功能(可修改要新增的東西)，公版東西有少要補，否則無法運作

--- testCshap/Controllers/MemberController.cs
+++ testCshap/Controllers/MemberController.cs
@@ -76,29 +76,41 @@ namespace testCshap.Controllers
             return RedirectToAction("Block_List");
         }
 
-        //public IActionResult Edit(int? id)
-        //{
-        //    DbDemoContext db = new DbDemoContext();
-        //    TCustomer x = db.TCustomers.FirstOrDefault(p => p.FId == id);
-        //    if (x == null)
-        //        return RedirectToAction("List");
-        //    return View(x);
-        //}
-        //[HttpPost]
-        //public IActionResult Edit(TCustomer uiCustomer)
-        //{
-        //    DbDemoContext db = new DbDemoContext();
-        //    TCustomer dbCustomer = db.TCustomers.FirstOrDefault(p => p.FId == uiCustomer.FId);
-        //    if (dbCustomer != null)
-        //    {
-        //        dbCustomer.FName = uiCustomer.FName;
-        //        dbCustomer.FPassword = uiCustomer.FPassword;
-        //        dbCustomer.FPhone = uiCustomer.FPhone;
-        //        dbCustomer.FEmail = uiCustomer.FEmail;
-        //        dbCustomer.FAddress = uiCustomer.FAddress;
-        //        db.SaveChanges();
-        //    }
-        //    return RedirectToAction("List");
-        //}
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TMember p)
+        {
+            DbShizukuDemoContext db = new DbShizukuDemoContext();
+            db.TMembers.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            DbShizukuDemoContext db = new DbShizukuDemoContext();
+            TMember x = db.TMembers.FirstOrDefault(p => p.FId == id);
+            if (x == null)
+                return RedirectToAction("List");
+            return View(x);
+        }
+        [HttpPost]
+        public IActionResult Edit(TMember uiCustomer)
+        {
+            DbShizukuDemoContext db = new DbShizukuDemoContext();
+            TMember dbCustomer = db.TMembers.FirstOrDefault(p => p.FId == uiCustomer.FId);
+            if (dbCustomer != null)
+            {
+                dbCustomer.FName = uiCustomer.FName;
+                dbCustomer.FPassword = uiCustomer.FPassword;
+                dbCustomer.FPhone = uiCustomer.FPhone;
+                dbCustomer.FEmail = uiCustomer.FEmail;
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
     }
 }
