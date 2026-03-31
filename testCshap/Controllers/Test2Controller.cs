c6ff9d6f7b6775ebead18ad58ae8c64dc440e2af
Sean <sealll40@gmail.com>
2026-03-31 11:13:39 +0800

    feat: create test2Controller

---
+++ testCshap/Controllers/Test2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testCshap.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
